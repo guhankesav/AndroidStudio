269ece822725479e2e4f52fe02d64428a7b005d7
guhankesav <47454368+guhankesav@users.noreply.github.com>
2021-03-04 22:52:35 +0530

    Update MainActivity.java

--- 11_ScrollView/MainActivity.java
+++ 11_ScrollView/MainActivity.java
@@ -3,9 +3,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-   
-   
    @Override
    protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
