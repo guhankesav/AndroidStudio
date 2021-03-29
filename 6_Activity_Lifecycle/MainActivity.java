2aff3b63bcc7645638e7b7853291e82ec511900b
guhankesav <47454368+guhankesav@users.noreply.github.com>
2021-03-29 18:34:35 +0530

    Update MainActivity.java

--- 6_Activity_Lifecycle/MainActivity.java
+++ 6_Activity_Lifecycle/MainActivity.java
@@ -8,12 +8,6 @@ public class MainActivity extends AppCompatActivity {
 
     
     
-    
-    
-    
-    
-    
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
