6107a91ce62766489c9dbd0f9b56f326c9b6b60d
guhankesav <47454368+guhankesav@users.noreply.github.com>
2021-05-03 10:05:00 +0530

    Update MainActivity.java

--- 11_ScrollView/MainActivity.java
+++ 11_ScrollView/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.a9_scrollview;
 import androidx.appcompat.app.AppCompatActivity;
+
+
 import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
    @Override
