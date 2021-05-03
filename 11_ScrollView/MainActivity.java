345213ad11f0c3de96643c4e71b0b34e52fdd174
guhankesav <47454368+guhankesav@users.noreply.github.com>
2021-05-03 10:04:49 +0530

    Update MainActivity.java

--- 11_ScrollView/MainActivity.java
+++ 11_ScrollView/MainActivity.java
@@ -4,12 +4,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
-      
-      
-      
-      
-     
-
        super.onCreate(savedInstanceState);
        //uncomment this line below to set the horizontal scroll view
        //setContentView(R.layout.activity_main);
