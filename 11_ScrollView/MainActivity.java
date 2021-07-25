b1b4c175605725ff03c9d1d28667cb579a3a7f60
Guhan K <47454368+guhankesav@users.noreply.github.com>
2021-07-25 13:53:45 +0530

    Update MainActivity.java

--- 11_ScrollView/MainActivity.java
+++ 11_ScrollView/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a9_scrollview;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
